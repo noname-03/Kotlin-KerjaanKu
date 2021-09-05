c02295ec8ca187bc06511027bea703e3dad13dd2
fahrurrozi <fahrurrozi.arul3@gmail.com>
2021-09-05 15:54:28 +0700

    undo package splash and main

--- app/src/main/java/com/example/kerjaanku/MainActivity.kt
+++ app/src/main/java/com/example/kerjaanku/MainActivity.kt
@@ -1,8 +1,7 @@
-package com.example.kerjaanku.view.splash.main
+package com.example.kerjaanku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.kerjaanku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
