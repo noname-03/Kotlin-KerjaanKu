4695c2c23c34ebf0cf9c7f1ee946384c7a4418ab
fahrurrozi <fahrurrozi.arul3@gmail.com>
2021-09-05 15:57:43 +0700

    fixs package splash and main

--- app/src/main/java/com/example/kerjaanku/views/main/MainActivity.kt
+++ app/src/main/java/com/example/kerjaanku/views/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kerjaanku
+package com.example.kerjaanku.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kerjaanku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
